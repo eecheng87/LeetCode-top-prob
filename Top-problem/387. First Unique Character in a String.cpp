f93aafacb35ccb07dc6dfc2126255818027d9116
eecheng <yucheng871011@gmail.com>
2021-04-13 13:42:47 +0800

    Change repo structure

---
+++ Top-problem/387. First Unique Character in a String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        char x;
+        int c[26] = {0};
+        
+        for(int i = 0; i < s.size(); i++)
+            c[s[i] - 'a']++;
+        for(int i = 0; i < s.size(); i++)
+            if(c[s[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+};
